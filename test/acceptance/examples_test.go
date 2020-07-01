f9ad4c5499a9c8901ff92d0c54be1c5efc2ad4d5
Simon Murray <spjmurray@yahoo.co.uk>
2020-07-01 16:17:08 +0100

    Upgrade Linter (#40)
    This allows Go 1.13 errors, that force static typing and not dynamic
    errors.  This should make classification and error reporting via the API
    more streamlined in future.
    
    Implements #35

--- test/acceptance/examples_test.go
+++ test/acceptance/examples_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package acceptance
+package acceptance_test
 
 import (
 	"encoding/base64"
